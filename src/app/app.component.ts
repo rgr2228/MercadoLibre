d6dac66752e8e5aa4253dc56f13219edebb5426e
rgr2228 <rgr2228@gmail.com>
2019-08-09 12:52:25 -0500

    Consumiendo api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Producto } from 'src/app/producto';
 import { PRODUCTOS } from 'src/app/lista-producto';
+import {HttpClient} from "@angular/common/http";
 
 
 @Component({
@@ -10,5 +11,22 @@ import { PRODUCTOS } from 'src/app/lista-producto';
 })
 export class AppComponent {
   title = 'app';
+  private resultado = null;
   productos = PRODUCTOS;
+
+  constructor(private http:HttpClient) { }
+
+  getProducts(prod){
+    this.http.get(`https://api.mercadolibre.com/sites/MCO/search?q=${prod.value}&access_token=APP_USR-1571889202014738-080917-26796bb0455c9b7e05d80cae249deb5f-381239892`)
+    .subscribe(
+      result=>{
+        this.resultado=result;
+        console.log(result);
+      },
+      error=>{
+        console.log("Error");
+      }
+    )
+  }
+
 }
--- src/app/producto.ts
+++ src/app/producto.ts
@@ -2,4 +2,5 @@ export class Producto {
     id: number;
     nombre: string;
     precio: string;
+    vendedor: string;
   }
